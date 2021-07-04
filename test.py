a39d5f2615dad54aac5f153e6b23cbb3aa9885a2
Ziv <1014863442@qq.com>
2021-07-05 00:30:44 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,9 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='1' # 这是默认的显示等级，显示所有信息
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='2' # 只显示 warning 和 Error
+os.environ["TF_CPP_MIN_LOG_LEVEL"]='3' # 只显示 Error
+import tensorflow as tf
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("Please install GPU version of TF")
